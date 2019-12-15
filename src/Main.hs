ff228ee5dffa2ea916cbd58b14cc30a9eab7e8a1
Yuya Kono <takohati0821@gmail.com>
2019-12-15 22:01:41 +0900

    変数を追加

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,51 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
+
+import Data.Functor.Identity
+import Control.Monad.State
 
 data Expr = Int Integer
           | Add Expr Expr
           | Sub Expr Expr
           | Mul Expr Expr
           | Div Expr Expr
+          | Var String
+          | Let String Expr Expr
   deriving (Eq, Ord, Show, Read)
 
-genExpr :: Expr -> String
-genExpr (Int x) = show x
-genExpr (Add e1 e2) = parens $ genExpr e1 <> "+" <> genExpr e2
-genExpr (Sub e1 e2) = parens $ genExpr e1 <> "-" <> genExpr e2
-genExpr (Mul e1 e2) = parens $ genExpr e1 <> "*" <> genExpr e2
-genExpr (Div e1 e2) = parens $ genExpr e1 <> "/" <> genExpr e2
+type GenState = [String]
 
-parens :: String -> String
-parens x = "(" <> x <> ")"
+genExpr :: MonadState [String] m => Expr -> m String
+genExpr (Int x) = pure $ show x
+genExpr (Add e1 e2) = parens $ do
+  e1' <- genExpr e1
+  e2' <- genExpr e2
+  pure $ e1' <> "+" <> e2'
+genExpr (Sub e1 e2) = parens $ do
+  e1' <- genExpr e1
+  e2' <- genExpr e2
+  pure $ e1' <> "-" <> e2'
+genExpr (Mul e1 e2) = parens $ do
+  e1' <- genExpr e1
+  e2' <- genExpr e2
+  pure $ e1' <> "*" <> e2'
+genExpr (Div e1 e2) = parens $ do
+  e1' <- genExpr e1
+  e2' <- genExpr e2
+  pure $ e1' <> "/" <> e2'
+genExpr (Var x) = pure x
+genExpr (Let x v e) = do
+  v' <- genExpr v
+  modify (<> ["int " <> x <> " = " <> v' <> ";"])
+  genExpr e
+
+parens :: Monad m => m String -> m String
+parens x = x >>= \x' -> pure $ "(" <> x' <> ")"
 
 compile :: Expr -> String
-compile x = "int main(void) { return " <> genExpr x <> "; }"
+compile x =
+  let (ret, lets) = runState (genExpr x) []
+  in "int main (void) { " <> mconcat lets <> "return " <> ret <> "; }"
 
 main :: IO ()
 main = do
