68074dcb9a243e6b5c3c3bf244ca50f7a8e2d81d
Yuya Kono <takohati0821@gmail.com>
2019-12-15 17:58:30 +0900

    implement arithmetic operations

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,27 @@
 module Main where
 
+data Expr = Int Integer
+          | Add Expr Expr
+          | Sub Expr Expr
+          | Mul Expr Expr
+          | Div Expr Expr
+  deriving (Eq, Ord, Show, Read)
+
+genExpr :: Expr -> String
+genExpr (Int x) = show x
+genExpr (Add e1 e2) = parens $ genExpr e1 <> "+" <> genExpr e2
+genExpr (Sub e1 e2) = parens $ genExpr e1 <> "-" <> genExpr e2
+genExpr (Mul e1 e2) = parens $ genExpr e1 <> "*" <> genExpr e2
+genExpr (Div e1 e2) = parens $ genExpr e1 <> "/" <> genExpr e2
+
+parens :: String -> String
+parens x = "(" <> x <> ")"
+
+compile :: Expr -> String
+compile x = "int main(void) { return " <> genExpr x <> "; }"
+
 main :: IO ()
 main = do
   src <- getContents
-  let value = read src :: Int
-  putStrLn $ "int main(void) { return " <> show value <> "; }"
+  let value = read src :: Expr
+  putStrLn $ compile value
